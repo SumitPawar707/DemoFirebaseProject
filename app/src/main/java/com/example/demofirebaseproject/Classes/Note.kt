0b2b3beabcd8f5926e930977dc4faec494486759
Sumit Pawar <sp260089@gmail.com>
2025-03-31 09:03:43 +0530

    adding and displaying multiple documents in single collection

--- app/src/main/java/com/example/demofirebaseproject/Classes/Note.kt
+++ app/src/main/java/com/example/demofirebaseproject/Classes/Note.kt
@@ -1,5 +1,10 @@
 package com.example.demofirebaseproject.Classes
 
+import com.google.firebase.firestore.Exclude
+
 data class Note(var title:String,var description:String){
     constructor() : this("","")// no-arg constructor needed
+
+    @Exclude
+    var id:String=""
 }
--- app/src/main/java/com/example/demofirebaseproject/MainActivity.kt
+++ app/src/main/java/com/example/demofirebaseproject/MainActivity.kt
@@ -11,23 +11,24 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.demofirebaseproject.Classes.Note
+import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ListenerRegistration
 import com.google.firebase.firestore.SetOptions
+import com.google.firebase.firestore.toObject
 
 class MainActivity : AppCompatActivity() {
     private lateinit var editTextTitle:EditText
     private lateinit var editTextDescription:EditText
     private lateinit var saveBtn:Button
-    private val db:FirebaseFirestore= FirebaseFirestore.getInstance()
-    private val docRef:DocumentReference=db.collection("Notebook").document("My FIrst Note")
-    private lateinit var displayTxtview:TextView
     private lateinit var loadBtn:Button
-    private lateinit var updateTitleBtn:Button
-    private lateinit var deleteDescBtn:Button
-    private lateinit var deleteNoteBtn:Button
+    private lateinit var displayTxtview:TextView
+
+   private val db:FirebaseFirestore=FirebaseFirestore.getInstance()
+    private val docRef:DocumentReference=db.collection("Notebook").document("My FIrst Note")
+    private val noteBookRef:CollectionReference=db.collection("Notebook")
     private lateinit var listener:ListenerRegistration //Stores a Firestore listener reference.
 
     private val KEY_TITLE="title"
@@ -41,54 +42,40 @@ class MainActivity : AppCompatActivity() {
 
         editTextTitle=findViewById(R.id.edit_text_title)
         editTextDescription=findViewById(R.id.edit_text_description)
-        saveBtn=findViewById(R.id.button_save)
+        saveBtn=findViewById(R.id.add_button)
         displayTxtview=findViewById(R.id.displaytxtview)
         loadBtn=findViewById(R.id.button_load)
-        updateTitleBtn=findViewById(R.id.button_updateTitle)
-        deleteDescBtn=findViewById(R.id.button_deleteDescription)
-        deleteNoteBtn=findViewById(R.id.button_deleteNote)
+
 
         saveBtn.setOnClickListener {
-            save()
+            addNote()
         }
         loadBtn.setOnClickListener {
-            loadData()
-        }
-        updateTitleBtn.setOnClickListener {
-            updateTitle()
-        }
-        deleteDescBtn.setOnClickListener {
-            deleteDescription()
-        }
-        deleteNoteBtn.setOnClickListener {
-            deleteNote()
+            loadNotes()
         }
 
     }
 
     override fun onStart() {
         super.onStart()
-       docRef.addSnapshotListener(this) { document, error ->
+        noteBookRef.addSnapshotListener { documentSnapshot, error ->
             error?.let {
                 return@addSnapshotListener
             }
-            document?.let {
-                if (it.exists()){
-
-                    //this is for adding using map
-                   // var title=it.getString(KEY_TITLE)
-                   // var descripiton=it.getString(KEY_DESCRIPTION)
-
-                    //this is using custom object
-                    val note=it.toObject(Note::class.java)
-
-                    displayTxtview.text="Title : ${note?.title} \n Description : ${note?.description}"
-                }else{
-                    displayTxtview.text=""
-                    Toast.makeText(this@MainActivity, "Error at loading data", Toast.LENGTH_SHORT).show()
+            //querydocumentsnapshot are gurenteed to exist so there is no need to check
+            documentSnapshot?.let {
+                var data=""
+                for (documentsnapshot in it){
+                    val note=documentsnapshot.toObject(Note::class.java)
+                    note.id=documentsnapshot.id
+                    var title=note.title
+                    var desc=note.description
+                    data += "Id: ${note.id} \n Title: $title \n Description: $desc"
                 }
+                displayTxtview.text=data
             }
         }
+
     }
 
 //    override fun onStop() {
@@ -97,7 +84,7 @@ class MainActivity : AppCompatActivity() {
 //    }                     //add( listener= docRef.addSnapshotListener { document, error ->)
                             //onstop can be done my automatically using "this" keyword in addSnapshotListener
 
-    private fun save(){
+    private fun addNote(){
         val title =editTextTitle.text.toString()
         val description=editTextDescription.text.toString()
         // Custom data object
@@ -107,42 +94,27 @@ class MainActivity : AppCompatActivity() {
 //        note.put(KEY_TITLE,title)  //here we put (key,value) so it should have been like (title,description) but I used default title
 //        note.put(KEY_DESCRIPTION,description)
 
-        docRef.set(note)
+        noteBookRef.add(note)
             .addOnSuccessListener {
                 Toast.makeText(this, "Note Added Succesfully", Toast.LENGTH_SHORT).show()
             }.addOnFailureListener {
                 Toast.makeText(this, "Error Note not added", Toast.LENGTH_SHORT).show()
             }
     }
-    private fun loadData(){
-        docRef.get()
-            .addOnSuccessListener { document ->
-                if (document.exists()){
-                    var title=document.getString(KEY_TITLE)
-                    var descripiton=document.getString(KEY_DESCRIPTION)
-                    displayTxtview.text="Title : $title \n Description : $descripiton"
-                }else{
-                    Toast.makeText(this@MainActivity, "Error at loading data", Toast.LENGTH_SHORT).show()
+    private fun loadNotes(){
+        noteBookRef.get()
+            .addOnSuccessListener { querySnapshot -> //querysnapshot contains all the documents
+                //querydocumentsnapshot are gurenteed to exist so there is no need to check
+                var data=""
+                for (documentSnapshot in querySnapshot){ //Loop through each document
+                    var note=documentSnapshot.toObject(Note::class.java) //convert to note object
+                    var title=note.title
+                    var desc=note.description
+                    data += "Title : $title \n Description: $desc \n \n"
                 }
-
-            }.addOnFailureListener {
-                Toast.makeText(this@MainActivity, "Error at loading data", Toast.LENGTH_SHORT).show()
+                displayTxtview.text=data
             }
+
     }
-    private fun updateTitle(){
-        var title=editTextTitle.text.toString()
-        val note = mutableMapOf<String, Any>()
-        note[KEY_TITLE]=title
-        docRef.set(note, SetOptions.merge()) //SetOptions.merge() this helps only to update provided data that is (title) /do not update anything other than title
-                                            //Use it only if you want to update few fields not entire date
-        //use docRef.update() to update entire data or fields
-    }
-    private fun deleteDescription(){
-        val note = mutableMapOf<String, Any>()
-        note[KEY_DESCRIPTION]=FieldValue.delete()
-        docRef.update(note)
-    }
-    private fun deleteNote(){
-        docRef.delete()
-    }
+
 }
