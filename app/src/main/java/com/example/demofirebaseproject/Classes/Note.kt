8ee4d8082c1a3edf5b52adf8a720387b70f4c4d9
Sumit Pawar <sp260089@gmail.com>
2025-04-04 12:18:12 +0530

    Firestore: Adding Arrays

--- app/src/main/java/com/example/demofirebaseproject/Classes/Note.kt
+++ app/src/main/java/com/example/demofirebaseproject/Classes/Note.kt
@@ -2,8 +2,12 @@ package com.example.demofirebaseproject.Classes
 
 import com.google.firebase.firestore.Exclude
 
-data class Note(var title:String,var description:String, var priority:Int){
-    constructor() : this("","",0)// no-arg constructor needed
+data class Note(
+    var title:String,
+    var description:String, var priority:Int,
+    var tags: MutableList<String>?
+){
+    constructor() : this("","",0,null)// no-arg constructor needed
 
     @Exclude
     var id:String=""
--- app/src/main/java/com/example/demofirebaseproject/MainActivity.kt
+++ app/src/main/java/com/example/demofirebaseproject/MainActivity.kt
@@ -10,18 +10,17 @@ import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.example.demofirebaseproject.Classes.Note
-import com.google.android.gms.tasks.Task
-import com.google.android.gms.tasks.Tasks
 import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ListenerRegistration
-import com.google.firebase.firestore.QuerySnapshot
 
 class MainActivity : AppCompatActivity() {
     private lateinit var editTextTitle:EditText
     private lateinit var editTextDescription:EditText
     private lateinit var editTextPriority:EditText
+    private lateinit var editTextTags:EditText
     private lateinit var saveBtn:Button
     private lateinit var loadBtn:Button
     private lateinit var displayTxtview:TextView
@@ -43,6 +42,7 @@ class MainActivity : AppCompatActivity() {
         editTextTitle=findViewById(R.id.edit_text_title)
         editTextDescription=findViewById(R.id.edit_text_description)
         editTextPriority=findViewById(R.id.edit_text_priority)
+        editTextTags=findViewById(R.id.edit_text_tags)
         saveBtn=findViewById(R.id.add_button)
         displayTxtview=findViewById(R.id.displaytxtview)
         loadBtn=findViewById(R.id.button_load)
@@ -54,7 +54,7 @@ class MainActivity : AppCompatActivity() {
         loadBtn.setOnClickListener {
             loadNotes()
         }
-        executeTransaction()
+        updateArray()
 
     }
 
@@ -76,8 +76,11 @@ class MainActivity : AppCompatActivity() {
             editTextPriority.setText("0")
         }
         val priority=editTextPriority.text.toString().toInt()
+
+        val tagsArray=editTextTags.text.toString().trim().split(",")
+        val tags= tagsArray.toMutableList()
         // Custom data object
-        val note=Note(title,description,priority)
+        val note=Note(title,description,priority,tags)
         //adding data using map
 //        val note= mutableMapOf<String,Any>()
 //        note.put(KEY_TITLE,title)  //here we put (key,value) so it should have been like (title,description) but I used default title
@@ -92,41 +95,26 @@ class MainActivity : AppCompatActivity() {
     }
     @SuppressLint("SuspiciousIndentation")
     private fun loadNotes(){
-        //Firestore Queries doesnt have "OR" operator and "NOT" operator (Ex : retrieve document not equal to  (priority =2)) It is not possible
-        val task1=noteBookRef.whereLessThan("priority",2)
-            .orderBy("priority")
-            .get()
-        val task2 = noteBookRef.whereGreaterThan("priority", 2)
-            .orderBy("priority")
-            .get()
-
-        var allTasks:Task<List<QuerySnapshot>> = Tasks.whenAllSuccess(task1,task2)
-            allTasks.addOnSuccessListener {
-                var data=""
-                for (querySnapshot in it){
-                    for (documentsnapshot in querySnapshot){
-                        var note=documentsnapshot.toObject(Note::class.java)
-                        note.id=documentsnapshot.id
-                        var title=note.title
-                        var desc=note.description
-                        var priority=note.priority
-                        data += "Id: ${note.id} \n Title: $title \n Description: $desc \n Priority : $priority \n\n"
-                    }
+        noteBookRef.get().addOnSuccessListener { querysnapshot ->
+            var data=""
+            for (documentSnapshot in querysnapshot){
+                val note=documentSnapshot.toObject(Note::class.java)
+                note.id=documentSnapshot.id
+                data += "\n ID : ${note.id}"
+                for (tag in note.tags!!){
+                    data += "\n $tag"
                 }
-                displayTxtview.text=data
             }
-    }
-    private fun executeTransaction(){
-        //This FUnction is next version of batch()
-        //It perform operation on whole batch but additionaly it performs operation on current value
-        //if the user enter new value during process of executing batch operation then it will get that new value as (current value)
+            displayTxtview.text=data
 
-       db.runTransaction{
-           val docRef = noteBookRef.document("New Note")
-           val snapshot=it.get(docRef)
-           val newPriority=snapshot.getLong("priority")?.plus(1)
-           it.update(docRef,"priority",newPriority)
-       }
+        }
+
+
+    }
+    private fun updateArray(){
+        noteBookRef.document("WGp62k5ia8MAXIFZG9EL")
+            //.update("tags",FieldValue.arrayUnion("New tag")) //adding new tag(element) to tags array
+            .update("tags",FieldValue.arrayRemove("tag3"))  //deleting tag3(element) from tags array
     }
 
 
